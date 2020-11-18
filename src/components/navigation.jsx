96cc6d2900d37c80a15bade0afdf25398490f6b4
kateryna-g <kat.hryhorieva@gmail.com>
2020-11-18 22:48:56 +0200

    1. change some styles 2. change description in navigation 3. change button colors 4. hide the testimonials block

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -19,7 +19,7 @@ export class Navigation extends Component {
               <span className="icon-bar"></span>{" "}
             </button>
             <a className="navbar-brand page-scroll" href="#page-top">
-              React Landing Page
+              My Landing Page
             </a>{" "}
           </div>
 
@@ -30,37 +30,33 @@ export class Navigation extends Component {
             <ul className="nav navbar-nav navbar-right">
               <li>
                 <a href="#features" className="page-scroll">
-                  Features
+                  Характеристики
                 </a>
               </li>
               <li>
                 <a href="#about" className="page-scroll">
-                  About
+                  Описание
                 </a>
               </li>
               <li>
                 <a href="#services" className="page-scroll">
-                  Services
+                  Сервисы
                 </a>
               </li>
               <li>
                 <a href="#portfolio" className="page-scroll">
-                  Gallery
+                  Галерея
                 </a>
               </li>
-              <li>
-                <a href="#testimonials" className="page-scroll">
-                  Testimonials
-                </a>
-              </li>
+
               <li>
                 <a href="#team" className="page-scroll">
-                  Team
+                  Команда
                 </a>
               </li>
               <li>
                 <a href="#contact" className="page-scroll">
-                  Contact
+                  Контакты
                 </a>
               </li>
             </ul>
